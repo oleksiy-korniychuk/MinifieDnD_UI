5e9c162bfda13d16027fb57deb5706f34942e309
Korniychuk, Oleksiy <oleksiy2@illinois.edu>
2021-01-03 00:18:24 -0600

    added creature component that now displays all creatures in a given biome/location combination

--- src/Worldbuilding/worldbuildingPage.js
+++ src/Worldbuilding/worldbuildingPage.js
@@ -34,6 +34,7 @@ function  WorldbuildingSteps() {
 function Flow2() {
     const [biomes, setBiomes] = React.useState([]);
     const [locations, setLocations] = React.useState([]);
+    const [creatures, setCreatures] = React.useState([]);
     const [selectedBiome, setSelectedBiome] = React.useState("");
     const [selectedLocation, setSelectedLocation] = React.useState("");
 
@@ -59,12 +60,24 @@ function Flow2() {
         fetchData();
     }, [selectedBiome]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                "http://minifiednd.com:8880/minifiednd_api/flow2?biome=" + selectedBiome + "&location=" + selectedLocation,
+            );
+            setCreatures(result.data);
+        };
+
+        fetchData();
+    }, [selectedBiome, selectedLocation]);
+
     return (
         <div>
             <BiomeList biomes={biomes} set={setSelectedBiome}/>
             {selectedBiome}
             <LocationList locations={locations} set={setSelectedLocation} />
             {selectedLocation}
+            <CreatureList creatures={creatures} />
         </div>
     );
 }
@@ -120,10 +133,44 @@ function LocationList(props) {
                 <AccordionActions>
                     <Button
                         size="small"
+                        onClick={() => {}}
+                        disabled={false}
+                    >Back</Button>
+                    <Button
+                        size="small"
                         color="primary"
                         onClick={setLocation}
                         disabled={false}
                     >Submit</Button>
+                </AccordionActions>
+            </Accordion>
+        </div>
+    );
+}
+
+function CreatureList(props) {
+    return (
+        <div>
+            <Accordion>
+                <AccordionSummary>
+                    <Typography>Creatures:</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <EntityList items={props.creatures.map((creature) => creature.name)} />
+                </AccordionDetails>
+                <Divider />
+                <AccordionActions>
+                    <Button
+                        size="small"
+                        onClick={() => {}}
+                        disabled={false}
+                    >Back</Button>
+                    <Button
+                        size="small"
+                        color="primary"
+                        onClick={() => {}}
+                        disabled={false}
+                    >Refresh</Button>
                 </AccordionActions>
             </Accordion>
         </div>
