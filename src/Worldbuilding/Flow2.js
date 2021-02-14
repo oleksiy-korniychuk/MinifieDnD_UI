0228cabc7c271a3110feb8a8bfa21360be3d2717
Oleksiy Korniychuk <okorniychuk2@gmail.com>
2021-02-14 17:18:01 -0600

    cleaned up the redesigned flow2 and made sure it functions, and on next the correct section highlights

--- src/Worldbuilding/Flow2.js
+++ src/Worldbuilding/Flow2.js
@@ -2,20 +2,19 @@ import React, { useEffect } from 'react';
 import '../index.css';
 import { EntityList } from '../Shared/AccordionSteps';
 import { Accordion, AccordionDetails, AccordionSummary, AccordionActions } from '@material-ui/core';
-import { Typography, Divider, Button } from '@material-ui/core'
+import { Typography, Divider, Button, Paper } from '@material-ui/core'
 import Grid from '@material-ui/core/Grid';
 
 // Redesign Imports
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
-import StepButton from '@material-ui/core/StepButton';
 
 // CONSTANTS
 const axios = require('axios').default;
 const NUM_BIOMES = 5;
 const NUM_LOCATIONS = 8;
-const NUM_CREATURES = 20;
+const NUM_CREATURES = 15;
 
 // COMPONENTS
 function Flow2() {
@@ -55,13 +54,13 @@ function Flow2() {
     return (
         <div>
             <Grid container spacing={1}>
-                <Grid item sm={6}>
+                <Grid item md={6} xs={12}>
                     <BiomeList biomes={biomes} set={submitBiome} />
                 </Grid>
-                <Grid item sm={6}>
+                <Grid item md={6} xs={12}>
                     <LocationList locations={locations} set={setSelectedLocation} />
                 </Grid>
-                <Grid item sm={12}>
+                <Grid item md={12} xs={12}>
                     <CreatureList creatures={creatures} />
                 </Grid>
             </Grid>
@@ -76,6 +75,8 @@ function Flow2_Redesign() {
     const [selectedBiome, setSelectedBiome] = React.useState("");
     const [selectedLocation, setSelectedLocation] = React.useState("");
     const [componentDidMount, setComponentDidMount] = React.useState(false);
+    const [selectedBiomeIndex, setSelectedBiomeIndex] = React.useState(null);
+    const [selectedLocationIndex, setSelectedLocationIndex] = React.useState(null);
 
     const [activeStep, setActiveStep] = React.useState(0);
     const steps = getSteps();
@@ -92,10 +93,17 @@ function Flow2_Redesign() {
         setActiveStep(0);
       };
 
-    const submitBiome = (biome) => {
-        setSelectedBiome(biome);
+    const submitBiome = (biome_index) => {
+        setSelectedBiomeIndex(biome_index);
+        setSelectedBiome(biomes[biome_index].name);
+        setSelectedLocationIndex(null);
         setSelectedLocation("");
         setCreatures([]);
+    }
+
+    const submitLocation = (location_index) => {
+        setSelectedLocationIndex(location_index);
+        setSelectedLocation(locations[location_index].name);
     }
 
     const fetchData = async (endpoint, setData) => {
@@ -120,55 +128,61 @@ function Flow2_Redesign() {
 
 
     return (
-        <div>
-          <Stepper activeStep={activeStep} alternativeLabel>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-          <div>
-            {activeStep === steps.length ? (
-              <div>
-                <Typography>All steps completed</Typography>
-                <Button onClick={handleReset}>Reset</Button>
-              </div>
-            ) : (
-              <div>
-                <Typography>{getStepContent(activeStep)}</Typography>
-                <Grid container spacing={1}>
-                    <Grid item sm={4}>
-                        <BiomeList biomes={biomes} set={submitBiome} />
-                    </Grid>
-                    <Grid item sm={4}>
-                        <LocationList locations={locations} set={setSelectedLocation} />
-                    </Grid>
-                    <Grid item sm={4}>
-                        <CreatureList creatures={creatures} />
-                    </Grid>
-                </Grid>
+        <Grid container spacin={1}>
+            <Grid item xs={12}>
+                <Stepper activeStep={activeStep} alternativeLabel>
+                    {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                    ))}
+                </Stepper>
+            </Grid>
+            <Grid item xs={12}>
+                {activeStep === steps.length ? (
                 <div>
-                  <Button
-                    disabled={activeStep === 0}
-                    onClick={handleBack}
-                  >
-                    Back
-                  </Button>
-                  <Button variant="contained" color="primary" onClick={handleNext}>
-                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                  </Button>
+                    <Paper>
+                        <Typography>All steps completed</Typography>
+                        <Button onClick={handleReset}>Reset</Button>
+                    </Paper>
                 </div>
-              </div>
-            )}
-          </div>
-        </div>
-    );
-
-    return (
-        <div>
-            
-        </div>
+                ) : (
+                <div>
+                    <Paper>
+                        <Typography>{getStepContent(activeStep)}</Typography>
+                    </Paper>
+                    <Grid container spacing={1}>
+                        <Grid item md={4} xs={12}>
+                            <Paper variant={activeStep === 0? "outlined":""}>
+                                <EntityList items={biomes.map((biome) => biome.name)} onSelect={submitBiome} selectedIndex={selectedBiomeIndex}/>
+                            </Paper>
+                        </Grid>
+                        <Grid item md={4} xs={12}>
+                            <Paper variant={activeStep === 1? "outlined":""}>
+                                <EntityList items={locations.map((location) => location.name)} onSelect={submitLocation} selectedIndex={selectedLocationIndex}/>
+                            </Paper>
+                        </Grid>
+                        <Grid item md={4} xs={12}>
+                            <Paper variant={activeStep === 2? "outlined":""}>
+                                <EntityList items={creatures.map((creature) => creature.name)} onSelect={()=>{}} />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                    <div>
+                        <Button
+                            disabled={activeStep === 0}
+                            onClick={handleBack}
+                        >
+                            Back
+                        </Button>
+                        <Button variant="contained" color="primary" onClick={handleNext}>
+                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                        </Button>
+                    </div>
+                </div>
+                )}
+            </Grid>
+        </Grid>
     );
 }
 
